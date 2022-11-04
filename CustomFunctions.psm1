07b04b04b36eb4bdf4ff15d1e9c3451a303f8725
chrisgoode83@gmail.com <chrisgoode83@gmail.com>
2022-11-04 15:52:48 -0500

    Initial Commit - NOT FOR CONSUMPTION

---
+++ CustomFunctions.psm1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    A collection of misc. custom functions to make life easier
+
+.EXAMPLE
+    
+
+.INPUTS
+    
+
+.OUTPUTS
+    
+
+.NOTES
+    Guaranteed to be the best free Error Handler Module or DOUBLE your money back! YOU CAN'T LOSE!
+
+.LINK
+    https://github.com/cgoode01/module-CustomFunctions/
+#>
+
